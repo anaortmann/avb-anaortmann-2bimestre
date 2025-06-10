e64aeaf0c7e2ea68b6a72ea3423738f2b692c9a8
anaortmann <ana.baracat@eaportal.org>
2025-06-09 21:52:50 -0300

    9-

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 p-4 text-white flex gap-4">
+    <nav className="w-full bg-blue-600 text-white p-4 flex flex-wrap justify-between items-center">
       <Link to="/" className="hover:underline">Início</Link>
       <Link to="/cachorros" className="hover:underline">Cachorros</Link>
     </nav>
